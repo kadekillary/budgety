778afff4e2845bc04dbd9b1736dc371888fe2eb9
kadekillary <kadekillary@pm.me>
2021-02-01 05:42:58 -0800

    Remove: TypeDict from types -> streamlit failure

--- budgeotto/types.py
+++ budgeotto/types.py
@@ -1,20 +1,20 @@
-from typing import TypedDict, List
+from typing import List
 
 from numpy import float64
 
 
-class CurveAttrs(TypedDict):
+class CurveAttrs:
     coef: float64
     intercept: float64
     median_spend: float64
 
 
-class Curves(TypedDict):
+class Curves:
     variable: str
     data: CurveAttrs
 
 
-class SpendConstr(TypedDict):
+class SpendConstr:
     variable: str
     # [Lower, Upper] bounds
     spend_bounds: List[float]
