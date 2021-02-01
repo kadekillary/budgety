e4350cf2ce3fce2a4a4bfd388e3c959a89ebb2b2
kadekillary <kadekillary@pm.me>
2021-02-01 07:23:38 -0800

    Update: TypedDict -> namedtuple, python3.8 necessary

--- budgeotto/types.py
+++ budgeotto/types.py
@@ -1,20 +1,29 @@
-from typing import TypedDict, List
+from typing import List
+from collections import namedtuple
 
 from numpy import float64
 
 
-class CurveAttrs(TypedDict):
-    coef: float64
-    intercept: float64
-    median_spend: float64
+CurveAttrs = namedtuple("CurveAttrs", ["coef", "intercept", "median_spend"])
+
+#  class CurveAttrs(TypedDict):
+#  coef: float64
+#  intercept: float64
+#  median_spend: float64
 
 
-class Curves(TypedDict):
-    variable: str
-    data: CurveAttrs
+Curves = namedtuple("Curves", ["variable", "data"])
 
 
-class SpendConstr(TypedDict):
-    variable: str
-    # [Lower, Upper] bounds
-    spend_bounds: List[float]
+#  class Curves(TypedDict):
+#  variable: str
+#  data: CurveAttrs
+
+
+SpendConstr = namedtuple("SpendConstr", ["variable", "spend_bounds"])
+
+
+#  class SpendConstr(TypedDict):
+#  variable: str
+#  # [Lower, Upper] bounds
+#  spend_bounds: List[float]
